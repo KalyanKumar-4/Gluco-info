cab492d7080cc89eebf27d4dd489ae71e96cf39b
KalyanKumar-4 <56481727+KalyanKumar-4@users.noreply.github.com>
2021-04-28 22:05:26 +0530

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,30 @@
+#include "Node.h"
+#include <iostream>
+
+//Default Constructor
+Node::Node()
+ {
+     data = NULL;
+     next = NULL;
+ }
+
+
+ //Constructor with a value , but no pointer to next
+Node::Node(double newData)
+{
+    data =  newData;
+    next = NULL;
+}
+
+ //Constructor with value and next pointer
+Node::Node(double newData, Node* newNext)
+{
+    data =  newData;
+    next = newNext;
+}
+
+//Accessors for node data
+double Node::getData() const
+{
+    return data;
+}
